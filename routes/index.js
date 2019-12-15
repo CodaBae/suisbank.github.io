319adb16b361f0b2d8629d58cdb7f0a78816c87b
codabae <shalom.mat40@gmail.com>
2019-12-15 00:59:53 -0500

    try

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,9 @@ router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
   res.render('dashboard', {
-    user: req.user 
+    user: req.user,
+    
+    
   })
 );
 
