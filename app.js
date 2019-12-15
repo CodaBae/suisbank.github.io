357bed5201d77276ecc79d57701da825636ce994
codabae <shalom.mat40@gmail.com>
2019-12-15 00:54:52 -0500

    debug

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.use(function(req, res, next) {
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
-const PORT = process.env.PORT || 6020;
+const PORT = process.env.PORT || 6030;
   
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
